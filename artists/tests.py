a6c8836bdb67af227c38b377dbe818dc075044f8
Friedrich Delgado <delgado@dfn-cert.de>
2025-04-07 11:04:24 +0200

    Test that we can create an artist on the artists list endpoint
    Not exactly a unit test? Ok.

--- artists/tests.py
+++ artists/tests.py
@@ -1,6 +1,16 @@
 import pytest
+from rest_framework.reverse import reverse
+from rest_framework.test import APIClient
 
-# Create your tests here.
+@pytest.fixture
+def apiclient():
+    return APIClient()
 
-def test_stuff():
-    assert False
+
+# pytestmark = [pytest.mark.django_db]
+class TestArtistsList:
+
+    def test_create(self, apiclient):
+        response = apiclient.post(reverse("api:artists"),
+                                  {'name': 'The Beatles'})
+        assert response.status_code == 201
