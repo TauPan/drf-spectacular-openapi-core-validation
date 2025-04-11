78aa86cf1ee0747bf1daa66682e9d3105bac0588
Friedrich Delgado <delgado@dfn-cert.de>
2025-04-11 11:55:59 +0200

    Add slug_id field with a RegexValidator
    This gets automatically verified in the REST API, **and** in the admin
    forms and also is documented in both. Not the use-case I was looking for.
    
    I'm currently struggling hard to find a legitimate use-case that
    actually requires the use of the openapi validation I have in mind.
    
    Might be something that actually requires validating two (or more)
    fields together, i.e. would normally require python code but can also
    be expressed in openapi?
    
    But can't that be expressed in a validator that would be automatically
    documented in this way as well?
    
    Also there may be examples that can **not** be expressed in openapi,
    but can be expressed in python. In that case the fallback would be to
    explain it in the help_text/description and openapi validation would
    not work at all.

--- artists/models.py
+++ artists/models.py
@@ -1,10 +1,28 @@
+from django.contrib import admin
+from django.core.validators import RegexValidator
 from django.db import models
 
 import pycountry
 
-COUNTRY_CHOICES = [(p.alpha_2, p.name) for p in pycountry.countries]
+COUNTRY_CHOICES = [(p.alpha_2,
+                    p.name) for p in pycountry.countries]
 
 # Create your models here.
 class Artist(models.Model):
     name = models.TextField(unique=True)
-    country = models.CharField(max_length=2, choices=COUNTRY_CHOICES)
+    country = models.CharField(
+        max_length=2, choices=COUNTRY_CHOICES)
+    slug_id = models.CharField(
+        unique=True,
+        help_text=(
+            'A (possibly empty) name in lower case asci alphabet letters'
+            ' or underscores [a-z_] followed by an 8 digit lowercase hex'
+            ' number. (e.g.: "the_the-e461ca37")'),
+        max_length=32,
+        validators=[RegexValidator(r'[a-z_]{0,23}-[0-9a-f]{8}')])
+
+    def __str__(self):
+        return f'{self.name} ({self.country})'
+
+
+admin.site.register(Artist)
--- artists/tests.py
+++ artists/tests.py
@@ -17,7 +17,8 @@ class TestArtistsList:
     def test_create(self, apiclient):
         response = apiclient.post(reverse("api:artists-list"),
                                   {'name': 'The Beatles',
-                                   'country': 'GB'})
+                                   'country': 'GB',
+                                   'slug_id': 'beatles-42319fb2'})
         assert response.status_code == 201
         assert models.Artist.objects.count() == 1
         artist = models.Artist.objects.get()
