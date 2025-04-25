e9c48b49931ae3d4a869da97a6b9f9ae22b0fc86
Friedrich Delgado <delgado@dfn-cert.de>
2025-04-25 14:41:27 +0200

    Add serializer fields for from_addr, From, and To

--- mails/serializers.py
+++ mails/serializers.py
@@ -1,8 +1,12 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import EmailField, ListField, ModelSerializer
 import mails.models
 
 
 class MailSerializer(ModelSerializer):
+    from_addr = EmailField()  # aka Envelope-Address
+    From = EmailField()       # From Header-Field
+    To = ListField(child=EmailField(), allow_empty=False)
+
     class Meta:
         model = mails.models.Mail
         fields = '__all__'
