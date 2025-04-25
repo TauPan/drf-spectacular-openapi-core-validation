2ca41f92195c4e7ac2778395eb7368cd197888de
Friedrich Delgado <delgado@dfn-cert.de>
2025-04-25 14:06:04 +0200

    create Mail model

--- mails/models.py
+++ mails/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Mail(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    from_addr = models.TextField()
+    Subject = models.TextField(null=True)
+    From = models.TextField(null=True)
+    To = models.JSONField(null=True)
+    extra_headers = models.JSONField(null=True)
+    mail_body = models.TextField(null=True)
+    attachments = models.JSONField(null=True)
